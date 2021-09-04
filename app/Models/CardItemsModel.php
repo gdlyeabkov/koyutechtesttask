0ac1d69ac8e4115c96d8441485f9febb9c04a980
gdlyeabkov <gdlyeabkov@gmail.com>
2021-09-04 22:15:04 +0300

    init

---
+++ app/Models/CardItemsModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CardItemsModel extends Model {
+    protected $table = 'card_items';
+}
